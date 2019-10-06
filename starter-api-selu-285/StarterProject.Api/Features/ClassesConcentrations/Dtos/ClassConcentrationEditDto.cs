6105e58487ad2742115f4429bf74f1f028891948
Lorenzo Basili <oromis95@gmail.com>
2019-10-06 17:14:39 -0500

    Added Classes Concentrations DTOs

---
+++ starter-api-selu-285/StarterProject.Api/Features/ClassesConcentrations/Dtos/ClassConcentrationEditDto.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterProject.Api.Features.ClassesConcentrations.Dtos
+{
+    public class ClassConcentrationEditDto
+    {
+        public int Id { get; set; }
+        public string ClassId { get; set; }
+        public string ConcentrationId { get; set; }
+    }
+
+    public class ClassConcentrationEditDtoValidator : AbstractValidator<ClassConcentrationEditDto>
+    {
+        public ClassConcentrationEditDtoValidator()
+        {
+            RuleFor(x => x.ClassId)
+                .NotEmpty();
+
+            RuleFor(x => x.ConcentrationId)
+                .NotEmpty();
+        }
+    }
+}
---
+++ starter-api-selu-285/StarterProject.Api/Features/ClassesConcentrations/Dtos/ClassConcentrationGetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StarterProject.Api.Features.ClassesConcentrations.Dtos
+{
+    public class ClassConcentrationGetDto
+    {
+        public int Id { get; set; }
+        public string ClassId { get; set; }
+        public string ConcentrationId { get; set; }
+    }
+}
