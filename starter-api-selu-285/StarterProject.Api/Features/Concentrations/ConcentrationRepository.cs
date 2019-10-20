aa5ffdda217707e0a43c25af05aa24176f7632b3
Blaine Prestenbach <blaine.prestenbach@selu.edu>
2019-10-20 12:39:05 -0500

    Added crud commands for concentrations table
    Things that don't work:
    1) Unable to change the majorId FK once it's made(don't know why yet).
    2) If you create a concentration with a majorId that doesn't exist it breaks(similar to the get commands).

--- starter-api-selu-285/StarterProject.Api/Features/Concentrations/ConcentrationRepository.cs
+++ starter-api-selu-285/StarterProject.Api/Features/Concentrations/ConcentrationRepository.cs
@@ -1,12 +1,18 @@
 ﻿using StarterProject.Api.Data;
 using StarterProject.Api.Features.Concentrations.Dtos;
 using StarterProject.Api.Features.Users;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StarterProject.Api.Features.Concentrations
 {
     public interface IConcentrationRepository
     {
         ConcentrationGetDto CreateConcentration(ConcentrationCreateDto concentrationCreateDto);
+        ConcentrationGetDto GetConcentration(int concentrationId);
+        List<ConcentrationGetDto> GetAllConcentrations();
+        void DeleteConcentration(int ConcentrationId);
+        ConcentrationGetDto EditConcentration(int id, ConcentrationEditDto concentrationUpdateDto);
     }
     
     public class ConcentrationRepository : IConcentrationRepository
@@ -37,6 +43,59 @@ namespace StarterProject.Api.Features.Concentrations
             };
 
             return concentrationGetDto;
+        }
+
+        public ConcentrationGetDto GetConcentration(int concentrationId)
+        {
+            return _context
+                .Set<Concentration>()
+                .Select(x => new ConcentrationGetDto
+                {
+                    Id = x.Id,
+                    ConcentrationName = x.ConcentrationName,
+                    MajorId = x.MajorId
+                })
+                .FirstOrDefault(x => x.Id == concentrationId);
+        }
+
+        public List<ConcentrationGetDto> GetAllConcentrations()
+        {
+            return _context
+                .Set<Concentration> ()
+                .Select(x => new ConcentrationGetDto
+                {
+                    Id = x.Id,
+                    ConcentrationName = x.ConcentrationName,
+                    MajorId = x.MajorId
+                })
+                .ToList();
+        }
+
+        public ConcentrationGetDto EditConcentration(int Id, ConcentrationEditDto concentrationEditDto)
+        {
+            var concentration = _context.Set<Concentration>().Find(Id);
+
+
+            concentration.ConcentrationName = concentrationEditDto.ConcentrationName;
+            _context.SaveChanges();
+
+            var concentrationGetDto = new ConcentrationGetDto
+            {
+                Id = concentration.Id,
+                ConcentrationName = concentration.ConcentrationName,
+                MajorId = concentration.MajorId
+            };
+
+            concentrationGetDto.Id = concentration.Id;
+
+            return concentrationGetDto;
+        }
+
+        public void DeleteConcentration(int concentrationId)
+        {
+            var concentration = _context.Set<Concentration>().Find(concentrationId);
+            _context.Set<Concentration>().Remove(concentration);
+            _context.SaveChanges();
         }
     }
 }
---
+++ starter-api-selu-285/StarterProject.Api/Features/Concentrations/Dtos/ConcentrationEditDto.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace StarterProject.Api.Features.Concentrations.Dtos
+{
+    public class ConcentrationEditDto
+    {
+        public string ConcentrationName { get; set; }
+        public int MajorId { get; set; }
+    }
+
+    public class ConcentrationEditDtoValidator : AbstractValidator<ConcentrationEditDto>
+    {
+        public ConcentrationEditDtoValidator()
+        {
+            RuleFor(x => x.ConcentrationName)
+                .NotEmpty();
+        }
+    }
+}
