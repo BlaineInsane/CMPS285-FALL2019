cfb513c725b781502637405aca0c7c60153c786b
Blaine Prestenbach <blaine.prestenbach@selu.edu>
2020-01-03 21:18:19 -0600

    delete old database
    Next: update gitignore for a new database

--- starter-api-selu-285/StarterProject.Api/Features/Majors/Dtos/MajorGetDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StarterProject.Api.Features.Majors.Dtos
-{
-    public class MajorGetDto
-    {
-        public int Id { get; set; }
-        public string MajorName { get; set; }
-    }
-}
